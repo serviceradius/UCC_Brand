f3174ae0ad053ab1570b77540c02b56c3f61eb1c
Jaysen Parmar <jparmar@serviceradius.com>
2015-08-06 17:06:43 -0700

    UCC Brand Complete

---
+++ app.js
@@ -0,0 +1,29 @@
+//dependencies for each module used
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var handlebars = require('express3-handlebars');
+var app = express();
+
+
+var dotenv = require('dotenv');
+dotenv.load();
+
+//Route files to load
+var index = require('./routes/index');
+
+//Configures the Template engine
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.bodyParser());
+
+//Routes
+app.get('/', index.view);
+
+//set environment ports and start application
+app.set('port', process.env.PORT || 3000);
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+});
